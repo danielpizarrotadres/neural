6df7f5c6631b0b98414617ca2c2142dba559d4b3
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2026-07-29 19:53:50 -0400

    :sparkles: Add Input and Weight classes to build the Perceptron

--- perceptron.js
+++ perceptron.js
@@ -1,1 +1,17 @@
+class Input {
+  constructor(meaning, value) {
+    this.meaning = meaning;
+    this.value = value;
+  }
+}
 
+class Weight {
+  constructor(input, value) {
+    this.input = input;
+    this.value = value;
+  }
+}
+
+const x1 = new Input('Weather is good', 1);
+
+const w1 = new Weight(x1, 6);
